7d8222556ea08ba4fffd9f1cbfde7c5be217b7be
mekala sriramgopi <mekalasriramgopi@gmail.com>
2025-10-10 10:38:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+        print("Welcome to the Machine")
+
+    def deposit(self):
+        amount = float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\nAmount Deposited:", amount)
+
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance >= amount:
+            self.balance -= amount
+            print("\nYou Withdrew:", amount)
+        else:
+            print("\nInsufficient balance")
+
+    def display(self):
+        print("\nNet Available Balance =", self.balance)
+
+
+# Driver code
+    s = BankAccount()  # Create an object of BankAccount
+
+    s.deposit()        # Deposit money
+    s.withdraw()       # Withdraw money
+    s.display()        # Display balance
